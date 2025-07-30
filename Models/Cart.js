2caaeea6941084c30df848ebda0ecb74bed6d8bb
hala <halakmail95@gmail.com>
2025-07-30 17:23:55 +0300

    Add a blank line in Cart.js for improved readability

--- Models/Cart.js
+++ Models/Cart.js
@@ -11,6 +11,7 @@ const cartItemSchema = new mongoose.Schema({
 }, { _id: false });
 
 
+
 const cartSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
   guestId: { type: String, default: null, index: true },
